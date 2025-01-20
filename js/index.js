4fc1ac674b7a1659a0172027fb58f9ac7fe1e519
“KellyReisLee” <kristinaalvares28@gmail.com>
2025-01-20 00:27:33 +0000

    Adding some animation for Top-Veiculo section and Selecao Section

--- js/index.js
+++ js/index.js
@@ -3,16 +3,25 @@ document.addEventListener("DOMContentLoaded", () => {
 
   const observer = new IntersectionObserver(
     (entries, observer) => {
-      entries.forEach((entry) => {
+      entries.forEach((entry, index) => {
         if (entry.isIntersecting) {
-          entry.target.style.opacity = "1";
-          entry.target.style.transform = "translateY(0)";
+          // Adiciona o atraso de 1 segundo por elemento
+          setTimeout(() => {
+            entry.target.style.opacity = "1";
+            entry.target.style.transform = "translateY(0)";
+          }, index * 500); // Incrementa o atraso em 1 segundo por índice
           observer.unobserve(entry.target); // Para de observar depois que o elemento aparece
         }
       });
     },
-    { threshold: 0.2 } // Ajusta a porcentagem visível para acionar (10% visível)
+    { threshold: 0.2 } // Ajusta a porcentagem visível para acionar (20% visível)
   );
 
-  boxes.forEach((box) => observer.observe(box));
+  boxes.forEach((box) => {
+    // Inicia com estilos invisíveis
+    box.style.opacity = "0";
+    box.style.transform = "translateY(20px)";
+    observer.observe(box);
+  });
 });
+
